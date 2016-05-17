cee8d928258f1f17fb9acd12c0dc54f5e464bbab
KIMNAMMIN <namminkk@gmail.com>
2016-05-17 17:07:29 +0900

    sound

---
+++ 0429/Assets/Sound/sbe.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class sbe : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Destroy(gameObject, 3f);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
--- 0429/Assets/scripts/playershoot.cs
+++ 0429/Assets/scripts/playershoot.cs
@@ -4,17 +4,19 @@ using System.Collections;
 public class playershoot : MonoBehaviour {
 
     public GameObject bulletPrefab;
+    
 	// Use this for initialization
 	void Start () {
-	
+        
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
         if (Input.GetButtonDown("Fire1")){
-
+            
             Instantiate(bulletPrefab, transform.position, transform.rotation);
+ 
         }
 
 	}
