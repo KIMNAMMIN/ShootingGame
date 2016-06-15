98c3d7fd1bfd47a51ccf0fe0e16e198c0c56fed7
KIMNAMMIN <namminkk@gmail.com>
2016-06-15 18:32:41 +0900

    maybe final_teddybear rotation

--- 0111/Assets/scripts/Gamecontroller.cs
+++ 0111/Assets/scripts/Gamecontroller.cs
@@ -22,7 +22,7 @@ public class Gamecontroller : MonoBehaviour {
             for (int i = 0; i < 10; i++) { 
                
                 Vector3 SpawnPosition = new Vector3(Random.Range(-70,120), Random.Range(0,10), Random.Range(-20,170)); //어디에 리젠 시킬지.
-                Quaternion SpawnRotation = Quaternion.Euler(new Vector3(0, 0, 0)); //회전함수
+                Quaternion SpawnRotation = Quaternion.Euler(new Vector3(Random.Range(0, 360), Random.Range(0, 360), Random.Range(0, 360))); //회전함수
                 Instantiate(hazard, SpawnPosition, SpawnRotation);
                 yield return new WaitForSeconds(spawnWait);
             }
